b1fc1ffc18261f8d4352167d6199f9f87d5643b4
gfarrisi <gabriellafarrisi@gmail.com>
2023-02-26 13:15:06 -0700

    add cryptojs

--- src/components/Account.tsx
+++ src/components/Account.tsx
@@ -6,6 +6,7 @@ export type AccountDetails = {
   avatar: string;
   name: string;
   address: string;
+  balance?: number;
 };
 
 interface AccountProps {
@@ -17,16 +18,21 @@ export const Account: React.FunctionComponent<
 > = (props) => {
   const { account_details } = props;
   return (
-    <div className={styles.account_header}>
-      <Image
-        src={account_details.avatar}
-        width={35}
-        height={35}
-        alt="avatar"
-        className={styles.avatar}
-      />
-      <h2>{account_details.name}</h2>
-      <div></div>
+    <div>
+      <div className={styles.account_header}>
+        <Image
+          src={account_details.avatar}
+          width={35}
+          height={35}
+          alt="avatar"
+          className={styles.avatar}
+        />
+        <div>
+          <h2>{account_details.name}</h2>
+        </div>
+        <div></div>
+      </div>
+      {/* <h2>{account_details.balance}ETH</h2> */}
     </div>
   );
 };
