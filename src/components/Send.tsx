0659c7a7c95fb5f3fcc871b881e30a83f9fbee7b
gfarrisi <gabriellafarrisi@gmail.com>
2023-03-02 23:19:26 -0700

    (use-nework) add network hook

--- src/components/Send.tsx
+++ src/components/Send.tsx
@@ -19,7 +19,19 @@ export const Send: React.FunctionComponent<
       >
         ← Back
       </button>
+      <p>From:</p>
+      <div style={{ padding: 5 }} />
+      <p>{address}</p>
+      <div style={{ padding: 20 }} />
+      <p>To:</p>
+      <div style={{ padding: 5 }} />
       <input className={styles.input} />
+      <div style={{ padding: 10 }} />
+      <p>Amount:</p>
+      <div style={{ padding: 5 }} />
+      <input className={styles.input} />
+      <div style={{ padding: 20 }} />
+      <button className={styles.button}>Send</button>
     </div>
   );
 };
