e3ec0dbac4ac7700195d3273ade9fcf74f2940f6
gfarrisi <gabriellafarrisi@gmail.com>
2023-03-02 23:40:07 -0700

    changes

--- src/components/Account.tsx
+++ src/components/Account.tsx
@@ -1,5 +1,5 @@
+import { useWallet } from '@/hooks/use-wallet';
 import styles from '@/styles/Home.module.css';
-import Image from 'next/image';
 import React from 'react';
 
 export type AccountDetails = {
@@ -16,18 +16,19 @@ export const Account: React.FunctionComponent<
   React.PropsWithChildren<AccountProps>
 > = (props) => {
   const { account_details } = props;
+  const { address } = useWallet();
+
   return (
     <div>
       <div className={styles.account_header}>
-        <Image
-          src={account_details.avatar}
-          width={35}
-          height={35}
-          alt="avatar"
-          className={styles.avatar}
-        />
+        <svg viewBox="0 0 100 80" width="18" height="18">
+          <rect width="100" height="20"></rect>
+          <rect y="30" width="100" height="20"></rect>
+          <rect y="60" width="100" height="20"></rect>
+        </svg>
         <div>
-          <h2>{account_details.name}</h2>
+          <h4>{`SESAME`}</h4>
+          {/* <h4>{`${formatAddress(address, 4)}`}</h4> */}
         </div>
         <div></div>
       </div>
