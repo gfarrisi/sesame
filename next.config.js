1340460b3ebe15729c319c197c8aa31e66af10b7
dawsbot <dawsonbotsford@gmail.com>
2023-03-03 08:51:11 -0700

    Service Workers

--- next.config.js
+++ next.config.js
@@ -1,3 +1,7 @@
+const withPWA = require('next-pwa')({
+  dest: 'public',
+});
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
@@ -6,4 +10,4 @@ const nextConfig = {
   },
 };
 
-module.exports = nextConfig;
+module.exports = withPWA(nextConfig);
