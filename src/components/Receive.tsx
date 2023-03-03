360c24c26774eac7ddb2394cabe9c9eeead106b0
gfarrisi <gabriellafarrisi@gmail.com>
2023-03-03 01:13:25 -0700

    fix receive

--- src/components/Receive.tsx
+++ src/components/Receive.tsx
@@ -1,6 +1,5 @@
 import { ViewNames } from '@/pages';
 import styles from '@/styles/Home.module.css';
-import { colors } from '@/styles/themes';
 import { formatAddress } from '@/utils/helpers';
 import QRCode from 'qrcode.react';
 import { useWallet } from '../hooks/use-wallet';
@@ -17,17 +16,25 @@ export const Receive: React.FunctionComponent<
 
   return (
     <div style={{ padding: 10 }}>
-      <button
-        className={styles.button_unstyled}
-        onClick={() => setCurrentView('overview')}
-      >
-        ← Back
-      </button>
+      <div className={styles['flex-end']}>
+        <button
+          className={styles.button_unstyled}
+          onClick={() => setCurrentView('overview')}
+        >
+          Cancel
+        </button>
+      </div>
       <div className={styles.center}>
-        <QRCode value={address} size={256} />
-        <div className={styles.spacer} />
-        <div className={styles.spacer} />
-        <h4 style={{ color: colors.gray500 }}>{formatAddress(address, 6)}</h4>
+        <QRCode
+          value={address}
+          size={266}
+          style={{ borderRadius: 5, backgroundColor: 'white', padding: 10 }}
+        />
+      </div>
+      <div className={styles.spacer} />
+      <div className={styles.spacer} />
+      <div className={styles.center}>
+        <h4 style={{ color: 'black' }}>{formatAddress(address, 6)}</h4>
       </div>
     </div>
   );
